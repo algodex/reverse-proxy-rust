df8959f98ec657ee91abd86404aab01b84165643
Alexander Trefonas <atrefonas@gmail.com>
2022-12-08 11:24:19 -0500

    Add exmaple env

--- simple_server_client/client_test.spec.js
+++ simple_server_client/client_test.spec.js
@@ -1,5 +1,18 @@
 // const axios = require('axios');
 
+// Set reverse proxy .env to below:
+/*
+DEFAULT_CACHE_EXPIRY_TIME_SECS=6 # Default cache expiry time
+DEFAULT_REFRESH_WINDOW_SECS=12
+REQ_TIMEOUT=5 # Number of seconds to wait for an upstream request to time out
+# UPSTREAM_URL=http://host.docker.internal:3006
+#UPSTREAM_URL=http://localhost:3006
+UPSTREAM_URL=http://localhost:6000
+#UPSTREAM_URL=https://www.google.com
+
+CLEAR_CACHE_KEY=Secret # Set a secret for allow clearing the cache. clear-cache in header
+*/
+
 const runTest = async () => {
 
   const resp1 = await fetch('http://127.0.0.1:8000/long');
